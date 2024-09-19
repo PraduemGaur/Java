7b91c2dc4c53945bd85102ab2eb3d204f8bab524
XeNitSu <praduemgaur2023@gmail.com>
2024-09-20 00:14:48 +0530

    star

--- hello.java
+++ hello.java
@@ -1,7 +1,11 @@
-class hello
-{
-    public static void main(String []args)
-    {
-        System.out.println("My First Java.");
+public class hello {
+    public static void main(String[] args){
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+
     }
-};
+
+    } 
+    
