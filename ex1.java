82850560dbecf95954123478e5438b6581338251
XeNitSu <praduemgaur2023@gmail.com>
2024-09-26 21:09:02 +0530

    ex1(sum)

---
+++ ex1.java
@@ -0,0 +1,9 @@
+public class ex1 {
+    public static void main(String[] args) {
+        //variables
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
